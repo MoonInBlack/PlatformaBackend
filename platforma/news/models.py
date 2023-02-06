39514d296b43d1ea6d191921317ccd91a0dcb5f2
Oleg <platforma44@yandex.ru>
2023-02-06 14:57:59 +0300

    Initial Commit

--- platforma/news/models.py
+++ platforma/news/models.py
@@ -1,15 +1,10 @@
 from django.db import models
 
 
-class Post(models.Model):
+class Items(models.Model):
     """Страница поста как на сайте"""
     title = models.CharField(max_length=128)
-    description = models.CharField(max_length=128)
-    categories = models.CharField(max_length=11)
-    catname = models.CharField(max_length=11)
     image = models.ImageField(upload_to="images/categories/%Y/%m/%d/")
-    thumb = models.ImageField(upload_to="images/categories/%Y/%m/%d/")
-    blockquote = models.TextField(blank=True)
     content = models.TextField(blank=True)
     time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
